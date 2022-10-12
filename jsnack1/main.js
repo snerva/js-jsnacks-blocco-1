5a6a067a597ee83738d470754e8d4dbc4267e4b5
snerva <selenenerva@live.it>
2022-10-12 11:43:58 +0200

    dom print number

--- jsnack1/main.js
+++ jsnack1/main.js
@@ -4,9 +4,13 @@ console.log(userNumber1);
 const userNumber2 = prompt('Inserisci un altro numero:');
 console.log(userNumber2);
 
+const maxNumber = document.querySelector('h1');
+
 if (userNumber1 > userNumber2){
     console.log(userNumber1);
+    maxNumber.innerHTML = `${userNumber1}`;
 } else{
     console.log(userNumber2);
+    maxNumber.innerHTML = `${userNumber2}`;
 }
 //Il software stampa il maggiore. 
