f2762d47f39808ddd0717e9e872760c1dab2db74
snerva <selenenerva@live.it>
2022-10-12 12:13:44 +0200

    correction print

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -2,14 +2,16 @@
 const userFirstWord = prompt('Inserisci una parola:');
 const userSecondWord = prompt('Inserisci una seconda parola:');
 //Il software stampa prima la parola più corta, poi la parola più lunga 
-if (userFirstWord < userSecondWord){
+const word = document.querySelector('h1');
+
+if (userFirstWord > userSecondWord){
     console.log(userFirstWord);
 } else {
     console.log(userSecondWord);
 }
 
-
-
-
-
-
+if (userFirstWord < userSecondWord){
+    console.log(userFirstWord);
+} else {
+    console.log(userSecondWord);
+}
