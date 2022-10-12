8d8ee8ac4bba4c6982dce0f6979cd18139f0fffc
snerva <selenenerva@live.it>
2022-10-12 11:38:24 +0200

    user number

---
+++ jsnack1/main.js
@@ -0,0 +1,12 @@
+//L’utente inserisce due numeri in successione, con due prompt.
+const userNumber1 = prompt('Inserisci un numero:');
+console.log(userNumber1);
+const userNumber2 = prompt('Inserisci un altro numero:');
+console.log(userNumber2);
+
+if (userNumber1 > userNumber2){
+    console.log(userNumber1);
+} else{
+    console.log(userNumber2);
+}
+//Il software stampa il maggiore. 
