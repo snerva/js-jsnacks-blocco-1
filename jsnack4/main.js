25f3876a05d2b34051ab72acd6509aa9c776b20a
snerva <selenenerva@live.it>
2022-10-13 12:54:29 +0200

    snack 4

--- jsnack4/main.js
+++ jsnack4/main.js
@@ -8,14 +8,15 @@ const partyInvitedList = [
 console.log(partyInvitedList);
 //chiedi all’utente il suo nome
 const userName = prompt('Inserisci il tuo nome:');
-console.log(userName);
+
 //comunicagli se può partecipare o no alla festa.
 let i = 0;
 while (i < partyInvitedList.length){
     const thisInvited = partyInvitedList[i];
-
+    
     if (userName == thisInvited){
         console.log(userName);
+        document.querySelector('h1').innerHTML = 'Benvenuto al Party!';
     }  
     i++  
 }
