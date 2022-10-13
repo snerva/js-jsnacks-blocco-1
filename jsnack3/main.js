9737b85f8cfa2b0779439dc716c4313f44009f7f
snerva <selenenerva@live.it>
2022-10-13 12:01:06 +0200

    snack 3

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,8 +1,14 @@
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
 
-
 for (i=0; i<=10; i++){
     const userNumber = prompt('Inserisci un numero:');
     console.log(userNumber);
+
 }
 //Il programma stampa la somma di tutti i numeri inseriti.
+let i=0;
+while (i<=10){
+    const userNumber = prompt('Inserisci un numero:');
+    console.log(userNumber);
+    i++
+}
