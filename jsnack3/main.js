14047a87c40bbe4b424ad0fc79d3fd60a4a20d65
snerva <selenenerva@live.it>
2022-10-13 12:04:37 +0200

    correction

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,14 +1,17 @@
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
-
+let sum = 0;
 for (i=0; i<=10; i++){
     const userNumber = prompt('Inserisci un numero:');
     console.log(userNumber);
-
+    sum += userNumber;
 }
 //Il programma stampa la somma di tutti i numeri inseriti.
+
+letsum = 0;
 let i=0;
 while (i<=10){
     const userNumber = prompt('Inserisci un numero:');
     console.log(userNumber);
+    sum += userNumber;
     i++
 }
